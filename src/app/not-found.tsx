8ba0836404828e281a137f1f2cd3adaf8d32f511
joji000 <172300802+joji000@users.noreply.github.com>
2025-01-23 16:05:14 +0700

    refactor: not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,14 @@
 import NotFound from '@/components/NotFound'
+import { Box } from '@mui/material'
 
-const NotFoundPage = () => <NotFound />
+const NotFoundPage = () => 
+    <Box sx={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        }}>
+        <NotFound />
+    </Box>
 
 export default NotFoundPage
